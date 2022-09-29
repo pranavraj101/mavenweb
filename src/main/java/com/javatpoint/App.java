63ec2b409704e3ca1c52ca748bc2ab0009bbc9a9
pranavraj101 <sinhapranavraj10142@gmail.com>
2022-09-29 12:34:33 +0530

    Initial commit

---
+++ src/main/java/com/javatpoint/App.java
@@ -0,0 +1,35 @@
+package com.javatpoint;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.ipc.netty.http.server.HttpServer;
+
+import java.util.Optional;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+public class App {
+
+    static RouterFunction<ServerResponse> routes() {
+        return route(GET("/"), req -> ok().body(Flux.just("Hello", " ", "World!"), String.class));
+    }
+
+    public static void main(String[] args) throws Exception {
+        long begin = System.currentTimeMillis();
+        int port = Optional.ofNullable(System.getenv("PORT")) //
+                .map(Integer::parseInt) //
+                .orElse(8080);
+        HttpServer httpServer = HttpServer.create("0.0.0.0", port);
+        httpServer.startRouterAndAwait(routes -> routes.route(x -> true,
+                new ReactorHttpHandlerAdapter(toHttpHandler(App.routes()))), context -> {
+            long elapsed = System.currentTimeMillis() - begin;
+            LoggerFactory.getLogger(App.class).info("Started in {} seconds", elapsed / 1000.0);
+        });
+    }
+}
